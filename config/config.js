79fb416b47538ee7e1a827476e595432bc0be7ca
Wren <thitunguyen@my.lonestar.edu>
2023-06-29 19:49:28 -0500

    update for bugs in update and delete thoughts by id

--- config/config.js
+++ config/config.js
@@ -1,12 +1,10 @@
 module.exports = {
-    server: {
-      port: 3001, // The port on which your server will listen
-      // Other server configuration settings
-    },
-    database: {
-      connectionString: 'mongodb://localhost/social-network-api', // MongoDB connection string
-      // Other database configuration settings
-    },
-    // Add any other configuration settings specific to your application
-  };
-  
+  server: {
+    port: 3001, 
+
+  },
+  database: {
+    connectionString: 'mongodb://localhost/social-network-api', // MongoDB connection string
+   
+  },
+};
